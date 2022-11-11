1dd4935056d6a5262011b50224ef13590c92590e
Aarón J. Montes <aaronmontes_92@yahoo.es>
2022-11-11 17:48:23 -0600

    feat: 🎸 Adding layout for dashboard

---
+++ src/routes/dashboard/+layout.svelte
@@ -0,0 +1,49 @@
+<main class="container-fluid a-flex">
+  <aside>
+    <h3>Bienvenido</h3>
+    <nav>
+      <ul>
+        <li><a href="/dashboard">Inicio</a></li>
+        <li><a href="/dashboard/posts">Publicaciones</a></li>
+        <li><a href="/dashboard/categories">Categorias</a></li>
+        <li><a href="/dashboard/tags">Etiquetas</a></li>
+        <li><a href="/dashboard/pages">Páginas</a></li>
+        <li><a href="/dashboard/media">Multimedia</a></li>
+        <li><a href="/dashboard/comments">Comentarios</a></li>
+        <li><a href="/dashboard/users">Usuarios</a></li>
+      </ul>
+    </nav>
+    <button class="contrast outline" on:click={logout}>Cerrar sesión</button>
+  </aside>
+  <section class="container">
+    <slot />
+  </section>
+</main>
+
+<script>
+  import 'a-flexbox/a-flexbox.css';
+  import { tokenStore } from '../../store';
+  import { goto } from '$app/navigation';
+
+  function logout() {
+    tokenStore.set('');
+    goto('/');
+  }
+</script>
+
+<style>
+  aside {
+    width: 300px;
+    border-right: 1px solid lightgray;
+    height: 100vh;
+  }
+
+  section {
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+  }
+
+  h3 {
+    margin-bottom: 0.25rem !important;
+  }
+</style>
