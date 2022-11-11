8beb62044628c3f2e5a9b6555ed7f19284957e46
Aarón J. Montes <aaronmontes_92@yahoo.es>
2022-11-10 19:30:00 -0600

    refactor: 💡 Updating main page for project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,28 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<main class="container">
+  <nav>
+    <ul>
+      <li>
+        <a href="/">
+          <strong>Mi BLog</strong>
+        </a>
+      </li>
+    </ul>
+    <MainNavLinks />
+  </nav>
+  <section class="full-width full-height a-flex a-center a-align-items-center a-flex-direction-column">
+    <h1>Bienvenido a mi blog</h1>
+    <a href="/login" role="button">Iniciar Sesión</a>
+  </section>
+</main>
+
+
+<script lang="ts">
+  import 'a-flexbox/a-flexbox.css';
+  import MainNavLinks from "../components/MainNavLinks.svelte";
+</script>
+
+<style>
+  .full-height {
+    height: calc(100vh - 100px);
+  }
+</style>
