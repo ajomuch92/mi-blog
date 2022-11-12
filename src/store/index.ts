06cda071c0d9222dd59a1c956a4b0bd37ff014cc
Aarón J. Montes <aaronmontes_92@yahoo.es>
2022-11-11 19:21:35 -0600

    fix: 🐛 Fixing bug about undefined localstorage

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,10 +2,12 @@ import { writable } from 'svelte/store';
 
 export const tokenStore = writable('');
 
-tokenStore.subscribe((value) => {
-  if (value) {
-    localStorage.setItem('token', value);
-  } else {
-    localStorage.removeItem('token');
-  }
-})
+if (typeof localStorage !== 'undefined') {
+  tokenStore.subscribe((value) => {
+    if (value) {
+      localStorage.setItem('token', value);
+    } else {
+      localStorage.removeItem('token');
+    }
+  });
+}
