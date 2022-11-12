1ea5215aef5e503dc9ad7fdd96dcd6197cfee8a9
Aarón J. Montes <aaronmontes_92@yahoo.es>
2022-11-11 19:20:43 -0600

    feat: 🎸 Exporting all interfaces from index

---
+++ src/models/interfaces/index.ts
@@ -0,0 +1,4 @@
+export type { default as Category } from './Categories';
+export type { default as Columns } from './Colums';
+export type { default as Rendered } from './Pages';
+export type { default as Page } from './Pages';
